f8fddd9f4d0e0305e7b84102ddb7febbc27a3eaa
RYCHELS <kiagus.ryshad.skc@gmail.com>
2024-06-16 15:51:20 +0700

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,1 +1,10 @@
+import random
 
+def genn_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
